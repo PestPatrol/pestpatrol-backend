7692435ca9083eae65dd0810e85005aa446c9c92
Abimdn <abimdanu@gmail.com>
2024-05-31 10:02:45 +0700

    `feat:` handled GET requests for getting favorite/liked articles of a user

--- src/app/web.js
+++ src/app/web.js
@@ -4,7 +4,7 @@ const router = require('../routes/api');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(router);
 
 module.exports = app;
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,7 @@ const getHistoryData = require('../service/firestore/predictions-history');
 const getArticleDetail = require('../service/firestore/article-detail');
 const likeOrDislikeArticle = require('../service/firestore/like-dislike-article');
 const getArticles = require('../service/firestore/articles-list');
+const getLikedArticles = require('../service/firestore/liked-articles');
 
 const router = new express.Router();
 
@@ -23,10 +24,10 @@ router.post('/test', (req, res) => {
             status: 'success',
             // Read JSON request body
             receivedJson: req.body
-        })
-})
+        });
+});
 
-// Get history
+// TODO: Revise Get history based on new DB schema
 router.get('/predict/history/:userId', async (req, res) => {
     const userId = req.params.userId;
 
@@ -69,20 +70,29 @@ router.get('/articles', async (req, res) => {
     }
 });
 
-// Get article detail by articleId
-router.get('/articles/:articleId', async (req, res) => {
-    const articleId = req.params.articleId;
+// Get favorite (liked) articles
+router.get('/articles/liked', async (req, res) => {
+    const userId = req.query.userId;
+    console.log(`userId is ${userId}`);
+
+    if (!userId) {
+        return res.status(400)
+            .json({
+                status: 'fail',
+                message: 'No userId found!'
+            });
+    }
 
     try {
-        const articleDetailData = await getArticleDetail(articleId);
+        const articles = await getLikedArticles(userId);
 
         return res.status(200)
             .json({
                 status: 'success',
-                data: articleDetailData
+                data: articles
             });
     } catch (error) {
-        return res.status(error.statusCode)
+        return res
             .json({
                 status: 'fail',
                 message: error.message
@@ -107,13 +117,34 @@ router.post('/articles/like', async (req, res) => {
                     category: articleData.category,
                     isLiked: articleData.isLiked
                 }
-            })
+            });
     } catch (error) {
         return res
             .json({
                 status: 'fail',
                 message: error.message
-            })
+            });
+    }
+});
+
+// Get article detail by articleId
+router.get('/articles/:articleId', async (req, res) => {
+    const articleId = req.params.articleId;
+
+    try {
+        const articleDetailData = await getArticleDetail(articleId);
+
+        return res.status(200)
+            .json({
+                status: 'success',
+                data: articleDetailData
+            });
+    } catch (error) {
+        return res.status(error.statusCode)
+            .json({
+                status: 'fail',
+                message: error.message
+            });
     }
 });
 
@@ -134,13 +165,13 @@ router.post('/articles/:articleId/like', async (req, res) => {
                     category: articleData.category,
                     isLiked: articleData.isLiked
                 }
-            })
+            });
     } catch (error) {
         return res
             .json({
                 status: 'fail',
                 message: error.message
-            })
+            });
     }
 });
 
