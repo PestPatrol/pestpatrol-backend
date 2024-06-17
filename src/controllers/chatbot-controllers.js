3cd393faab077988fdf648eae21370c951a49e2f
Evan Laksana Wira Pratama <evanlwp123@gmail.com>
2024-06-18 02:02:18 +0700

    `add:` basic functionality for chatbot using temporary base model

--- src/controllers/chatbot-controllers.js
+++ src/controllers/chatbot-controllers.js
@@ -1,22 +1,22 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-require('dotenv').config();
+const { chatbotService } = require('../services/chatbots/chatbot');
 
 async function chatbotController(req, res) {
-  const { message } = req.body;
-  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
-  const result = await model.generateContent(message);
-
-  const responseText = await result.response;
-  console.log(responseText);
-  console.log(result)
-  return res.status(200).json({
-    success: true,
-    message: 'Success get response from chatbot',
-    data: {
-      response: responseText
-    }
-  });
+  try {
+    const responseText = await chatbotService(req);
+    return res.status(200).json({
+      success: true,
+      message: 'Success get response from chatbot',
+      data: {
+        response: responseText
+      }
+    });
+  } catch (error) {
+    return res.status(error.statusCode || 500)
+      .json({
+        success: false,
+        message: error.message,
+      });
+  }
 }
 
 module.exports = { chatbotController };
--- src/services/chatbots/chatbot.js
+++ src/services/chatbots/chatbot.js
@@ -1,21 +1,29 @@
-// const { GoogleGenerativeAI } = require('./google-generative-ai');
-// require('dotenv').config();
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+require('dotenv').config();
 
-// async function chatbotController(req, res) {
-//   const { message } = req.body;
-//   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY);
-//   const model = genAI.getModel('tunedModels/pestpatrol-aq4pa5hsyvo7');
-//   const result = await model.generateContent(message);
-  
-//   const responseText = await result.response.text;
+/*
+  NOTE: @google/generative-ai is still under development for managing permissions access to the tuned model.
+  ISSUE: https://github.com/google-gemini/generative-ai-js/issues/100 
+  TODO: Implement code to handle the tuned model once it becomes available
+*/
 
-//   return res.status(200).json({
-//     success: true,
-//     message: 'Success get response from chatbot',
-//     data: {
-//       response: responseText
-//     }
-//   });
-// }
+async function chatbotService(req) {
+  try{
+    const { message } = req.body;
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENAI_API_KEY);   
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const result = await model.generateContent(message);
+    const responseText = result.response.candidates[0].content.parts[0].text;
+    return preprocessMessage(responseText);
+  } catch(error){
+    console.log(error);
+    throw new Error(error.message || 'Failed to get response from chatbot');
+  }
+}
 
-// module.exports = { chatbotController };
+function preprocessMessage(message){
+  message = message.replace(/[\n\r\t\f\\"*#]/g, ' ');
+  return message;
+}
+
+module.exports = { chatbotService };
